a125ed334038d26f0426a1ad09313ff87ee400aa
Audrey <blueshadow1125@gmail.com>
2022-02-21 15:16:44 -0800

    made changes to lab06 files

--- lab06/2d_array.asm
+++ lab06/2d_array.asm
@@ -174,34 +174,39 @@ swap_rows: #takes in the address of the rows you want to swap and swaps them.
 # COPYFROMHERE - DO NOT REMOVE THIS LINE
 sort_by_row: 
     # a0 stores the array address, a1 and a2 store the size of row and column respectively
-    addiu $sp, $sp, -12
+    addiu $sp, $sp, -20
     sw $s0, 0($sp)
     sw $s1, 4($sp)
     sw $s2, 8($sp)
+    sw $s4, 12($sp)
+    sw $s5, 16($sp)
 
     move $t0, $a0
+    move $s1, $a1
+    move $s2, $a2
+
     li $t1, 0   # outer
     li $t2, 0   # inner
     # bubble sort outer:
     loop_outer:
-        ble $a2, $t1, end_outer     # reached end of outer loop
+        ble $s2, $t1, end_outer     # reached end of outer loop
         li $t2 0    # reset inner loop
 
         # bubble sort inner:
         loop_inner:
             add $t3, $t1, $t2
             addi $t3, $t3, 1
-            bge $t3, $a1, end_inner  # reached end of inner loop
+            bge $t3, $s1, end_inner  # reached end of inner loop
 
-            # getting address of row at j, and putting in s1
-            mult $a2, $t2
-            mflo $s1
+            # getting address of row at j, and putting in s4
+            mult $s2, $t2
+            mflo $s4
 
             li $t4, 4
-            mult $s1, $t4
-            mflo $s1
+            mult $s4, $t4
+            mflo $s4
 
-            add $s1, $s1, $s0
+            add $s4, $s4, $s0
 
             # preparing to call row avg for row at j
             addiu $sp, $sp, -16
@@ -210,7 +215,7 @@ sort_by_row:
             sw $t2, 8($sp)
             sw $ra, 12($sp)
             
-            move $a0 $s1
+            move $a0 $s4
             jal average_row
             move $t5 $v0
 
@@ -220,16 +225,16 @@ sort_by_row:
             lw $ra, 12($sp)
             addiu $sp, $sp, 16
 
-            # getting address of row at j+1, and putting in s2
+            # getting address of row at j+1, and putting in s5
             addi $t2, 1
-            mult $a2, $t2
-            mflo $s2
+            mult $s2, $t2
+            mflo $s5
             addi $t2, -1
 
-            mult $s2, $t4
-            mflo $s2
+            mult $s5, $t4
+            mflo $s5
 
-            add $s2, $s2, $s0
+            add $s5, $s5, $s0
 
             # preparing to call row avg for row at j+1
             addiu $sp, $sp, -16
@@ -238,7 +243,7 @@ sort_by_row:
             sw $t2, 8($sp)
             sw $ra, 12($sp)
             
-            move $a0, $s2
+            move $a0, $s4
             jal average_row
             move $t6, $v0
 
@@ -258,8 +263,8 @@ sort_by_row:
                 sw $t2, 8($sp)
                 sw $ra, 12($sp)
 
-                move $a0 $s1
-                move $a1 $s2
+                move $a0 $s4
+                move $a1 $s5
 
                 jal swap_rows
 
@@ -281,6 +286,8 @@ sort_by_row:
     lw $s0, 0($sp)
     lw $s1, 4($sp)
     lw $s2, 8($sp)
-    addiu $sp, $sp, 12
+    lw $s4, 12($sp)
+    lw $s5, 16($sp)
+    addiu $sp, $sp, 20
 
     jr $ra
